c4fd58f3aee63656a3a48227757354c38a7686dd
Honningbolden <hannibalmunch@gmail.com>
2024-04-10 11:46:49 +0200

    Add .gitignore and package.json files, and update style.css, index.html, and sketch.js

--- sketch.js
+++ sketch.js
@@ -1,67 +1,93 @@
 let scoops = [];
-let colorArray = [
-  "#F9AFD2",
-  "#FEE8E7",
-  "#D28475",
-  "#ADE9E2",
-  "#D4EF8F",
-  "#DEB0F6",
+let text = "Img/SVG/Asset 1.svg";
+let logo = "Img/hjemis-logo.png";
+let imageArray = [
+  "Img/1207840.png.avif",
+  "Img/1207851.png.avif",
+  "Img/1207855.png.avif",
+  "Img/36940035.png.avif",
+  "Img/champagnebrus.png",
+  "Img/Cornetto.png",
+  "Img/kungfu.png",
+  "Img/Nogger.png",
+  "Img/Solbaerstang.png",
+  "Img/frysepizza.png",
 ];
 
-function setup() {
-  createCanvas(400, 400);
+function preload() {
+  imageArray.forEach((image, index) => {
+    imageArray[index] = loadImage(image);
+  });
+  logo = loadImage(logo);
+  text = loadImage(text);
+}
 
-  let maxBubbles = 10;
+function setup() {
+  let canvas = createCanvas(842, 1191);
+  canvas.parent("canvasContainer");
+  imageMode(CENTER);
+  titleFont = textFont("breakers", 128);
+  subtitleFont = textFont("breakers", 42);
+
+  let maxBubbles = 30;
 
   for (let i = 0; i < maxBubbles; i++) {
     let x = random(width);
     let y = random(height);
-    let c = (colorArray[i % colorArray.length]);
+    let img = imageArray[i % imageArray.length];
+    let speedMult = random(0.5);
 
-    scoops.push(new Scoop(x, y, c));
+    scoops.push(new Scoop(x, y, img, speedMult));
   }
 }
 
 function draw() {
-  background(220);
+  background("#2DA9E1");
+
+  image(logo, width - height / 10, height - height / 10, height / 8, height / 8);
 
   for (scoop of scoops) {
     scoop.move();
     scoop.display();
   }
+
+  let displayWidth = width * 0.7; // 70% of the canvas width
+  let aspectRatio = text.width / text.height;
+  let displayHeight = displayWidth / aspectRatio;
+
+  image(text, (width) / 2, (height) / 2, displayWidth, displayHeight);
 }
 
-function newScoop() {
+function newScoop(imgSize) {
   let x = random(width);
-  let y = -50;
-  let c = (colorArray[floor(random(colorArray.length))]);
+  let y = -imgSize;
+  let img = imageArray[floor(random(imageArray.length))];
+  let speedMult = random(0.5);
 
-  scoops.push(new Scoop(x, y, c));
+  scoops.push(new Scoop(x, y, img, speedMult));
 }
 
 class Scoop {
-  constructor(x, y, c) {
-    this.color = c;
-    this.radius = 20;
-    this.speed = createVector(0, 1);
+  constructor(x, y, img, speedMult) {
+    this.image = img;
+    this.radius = height / 10;
+    this.speed = createVector(0, 1).mult(speedMult + 1);
     this.pos = createVector(x, y);
   }
 
   move() {
-    if (this.pos.y > height) {
+    if (this.pos.y > height + this.radius) {
       this.terminate();
-      newScoop();
+      newScoop(this.radius);
     }
     this.pos.add(this.speed);
   }
 
   display() {
-    noStroke();
-    fill(this.color);
-    circle(this.pos.x, this.pos.y, this.radius);
+    image(this.image, this.pos.x, this.pos.y, this.radius, this.radius);
   }
 
   terminate() {
-    scoops = scoops.filter(s => s !== this);
+    scoops = scoops.filter((s) => s !== this);
   }
 }
